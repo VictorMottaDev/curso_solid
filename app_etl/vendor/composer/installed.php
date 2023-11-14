ca851bc9a4b0086b80d128f053f544010dbe7027
VictorMottaDev <victor.motta.dev@gmail.com>
2023-11-14 07:45:29 -0300

    Modulo sobre ETL OCP

---
+++ app_etl/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ti/app_etl',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9025e77821060a9417f34a70547eb75afc936c40',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ti/app_etl' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9025e77821060a9417f34a70547eb75afc936c40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

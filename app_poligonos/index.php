e2102de364fe62db853e8ef6c07569770c7b748c
VictorMottaDev <victor.motta.dev@gmail.com>
2023-11-24 08:50:29 -0300

    Corrigindo para nao ferir o LSP

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -5,9 +5,15 @@ require __DIR__."/vendor/autoload.php";
 
 
 use App\Retangulo;
+use App\Quadrado;
 
 $retangulo = new Retangulo();
 $retangulo->setAltura(5);
 $retangulo->setLargura(10);
 
 echo '<h3> Area do retangulo: '.$retangulo->getArea().'</h3>';
+
+$quadrado = new Quadrado();
+$quadrado->setAltura(5);
+
+echo '<h3> Area do quadrado: '.$quadrado->getArea().'</h3>';

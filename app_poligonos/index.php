916f87c9b4ccc3c51e8bb818a8c95e36b9956356
VictorMottaDev <victor.motta.dev@gmail.com>
2023-11-28 08:36:04 -0300

    limpeza do codigo

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -18,18 +18,3 @@ echo '<pre>';
 print_r($poligono);
 echo '</pre>';
 echo '<h3> Area do retangulo: '.$poligono->getArea().'</h3>';
-
-
-
-/*
-$retangulo = new Retangulo();
-$retangulo->setAltura(5);
-$retangulo->setLargura(10);
-
-echo '<h3> Area do retangulo: '.$retangulo->getArea().'</h3>';
-
-$quadrado = new Quadrado();
-$quadrado->setAltura(5);
-
-echo '<h3> Area do quadrado: '.$quadrado->getArea().'</h3>';
-*/
